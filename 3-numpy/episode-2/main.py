f32cde1301b246800cd99177cdadae0a79d3ca5a
Mikhail Kalashnikov <kalashnikov.mm@phystech.edu>
2023-11-15 23:08:30 +0300

    fix 1

--- 3-numpy/episode-2/main.py
+++ 3-numpy/episode-2/main.py
@@ -6,12 +6,10 @@ fig, axs = plt.subplots(1, 3, figsize=(15, 5))
 
 for j in range(3):
     raw_data = np.loadtxt(os.path.join('signals', f'signal0{j + 1}.dat'))
-    res_data = np.zeros_like(raw_data)
-    for i in range(len(res_data)):
-        res_data[i] = np.mean(raw_data[max(0, i - 9):i + 1])
-
+    res_data = np.convolve(np.insert(raw_data, 0, np.zeros(9)), np.ones(10) / 10, mode='valid')[9:]
+    start_data = np.matmul(np.tri(9, 9), raw_data[:9]) / np.arange(1, 10)
     axs[j].plot(raw_data, label='Сырой сигнал')
-    axs[j].plot(res_data, label='После обработки')
+    axs[j].plot(np.append(start_data, res_data), label='После обработки')
     axs[j].set_title(f'Сигнал {j + 1}')
     axs[j].legend()
     axs[j].grid()
--- 4-scipy-sympy/episode-2/main.py
+++ 4-scipy-sympy/episode-2/main.py
@@ -2,10 +2,10 @@ import numpy as np
 import scipy as sp
 import matplotlib.pyplot as plt
 
-with open('system.txt') as file:
-    N = int(file.readline())
-    A, B = np.array_split(np.loadtxt(file.readlines()), [N], axis=0)
-    B = np.transpose(B)
+data = np.loadtxt('system.txt', skiprows=1)
+N = data.shape[1]
+A, B = np.array_split(data, [N], axis=0)
+B = np.transpose(B)
 
 plt.bar(np.arange(N), np.transpose(sp.linalg.solve(A, B))[0])
 plt.grid()

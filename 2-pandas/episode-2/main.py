23561e306fb7154e21c330d9b39e35ec569215e2
Mikhail Kalashnikov <kalashnikov.mm@phystech.edu>
2023-11-19 16:08:39 +0300

    pandas fix

--- 2-pandas/episode-2/main.py
+++ 2-pandas/episode-2/main.py
@@ -1,22 +1,10 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 
-dfg = pd.read_csv('flights.csv').groupby('CARGO')
-
-cargo = dfg.groups.keys()
-flights = dfg['CARGO'].count()
-prices = dfg['PRICE'].sum()
-weights = dfg['WEIGHT'].sum()
-
-fig, axs = plt.subplots(1, 3, figsize=(15, 5))
-
-axs[0].pie(flights, labels=cargo)
-axs[1].pie(prices, labels=cargo)
-axs[2].pie(weights, labels=cargo)
-
-axs[0].set_title('Total flights')
-axs[1].set_title('Total prices')
-axs[2].set_title('Total weights')
+dfg = pd.read_csv('flights.csv')
+dfg['COUNT'] = 1
+dfg = dfg.drop(['Unnamed: 0'], axis=1)
+dfg.groupby('CARGO').sum().plot.pie(subplots=True, figsize=(20, 10))
 
 plt.savefig('cargo.png')
 plt.show()
--- 2-pandas/episode-3/main.py
+++ 2-pandas/episode-3/main.py
@@ -6,23 +6,22 @@ import pandas as pd
 
 students = pd.read_excel(os.path.join('students', 'students_info.xlsx'))
 results = pd.read_html(os.path.join('students', 'results_ejudge.html'))[0]
-
 merged = students.merge(results, left_on='login', right_on='User')
-group_fac = merged.groupby('group_faculty')
-fac_names, fac_mean_solved = list(map(str, group_fac.groups.keys())), group_fac['Solved'].mean()
-group_out = merged.groupby('group_out')
-out_names, out_mean_solved = list(map(str, group_out.groups.keys())), group_out['Solved'].mean()
 
 _, axs = plt.subplots(1, 2, figsize=(10, 5))
+
+plt.sca(axs[0])
+merged.groupby('group_faculty')['Solved'].mean().plot.bar()
+plt.sca(axs[1])
+merged.groupby('group_out')['Solved'].mean().plot.bar()
+
 
 axs[0].grid()
 axs[1].grid()
 axs[0].set_axisbelow(True)
 axs[1].set_axisbelow(True)
-axs[0].bar(fac_names, fac_mean_solved)
-axs[1].bar(out_names, out_mean_solved)
-axs[0].set_xlabel('group')
-axs[1].set_xlabel('group')
+axs[0].set_xlabel('faculty group')
+axs[1].set_xlabel('out group')
 axs[0].set_ylabel('solved')
 axs[1].set_ylabel('solved')
 axs[0].set_title('Mean solved by faculty group')
@@ -33,9 +32,10 @@ plt.savefig('mean-solved.png')
 top = merged[(merged['G'] >= 10) | (merged['H'] >= 10)]
 
 _, axs = plt.subplots(1, 2, figsize=(10, 5))
-
-axs[0].pie(**dict(zip(['labels', 'x'], np.unique(np.array(top['group_faculty']), return_counts=True))))
-axs[1].pie(**dict(zip(['labels', 'x'], np.unique(np.array(top['group_out']), return_counts=True))))
+plt.sca(axs[0])
+top['group_faculty'].value_counts().plot.pie(ylabel='')
+plt.sca(axs[1])
+top['group_out'].value_counts().plot.pie(ylabel='')
 
 axs[0].set_title('Faculty groups top students came from')
 axs[1].set_title('Out groups top students went to')
